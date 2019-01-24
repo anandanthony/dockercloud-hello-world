b1fd8d96775ae97692e4f924d4fa36ff9b7ad6c2
anandanthony <anfranci@microsoft.com>
2019-01-24 20:38:53 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,3 +5,6 @@ CONF_FILE=/etc/nginx/nginx.conf
 sed -i "s/#LISTEN_PORT#/${LISTEN_PORT}/" ${CONF_FILE}
 
 php-fpm -d variables_order="EGPCS" && exec nginx -g "daemon off;"
+
+echo "Starting SSH ..."
+rc-service sshd start
